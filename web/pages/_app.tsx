6abcaeac06252f5b3dc4225c29f9af43f3a954de
Jay V <air@live.ca>
2023-01-20 13:33:57 -0500

    Formatting

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -23,32 +23,32 @@ const urql = createClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <Provider value={urql}>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>SST App</title>
-          <meta
-            name="theme-color"
-            media="(prefers-color-scheme: light)"
-            content="white"
-          />
-          <meta
-            name="theme-color"
-            media="(prefers-color-scheme: dark)"
-            content="hsl(216, 13%, 16%)"
-          />
-          <link rel="icon" href="/favicon.svg" />
-          <meta property="og:image" content="/share.png" />
-          <meta name="description" content="Built with SST" />
-        </Head>
-        <style jsx global>{`
-          :root {
-            --font-body: ${sans.style.fontFamily};
-            --font-code: ${code.style.fontFamily};
-            --font-heading: ${roboto.style.fontFamily};
-          }
-        `}</style>
-        <Component {...pageProps} />
-      </Provider>
+    <Provider value={urql}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>SST App</title>
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="white"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="hsl(216, 13%, 16%)"
+        />
+        <link rel="icon" href="/favicon.svg" />
+        <meta property="og:image" content="/share.png" />
+        <meta name="description" content="Built with SST" />
+      </Head>
+      <style jsx global>{`
+        :root {
+          --font-body: ${sans.style.fontFamily};
+          --font-code: ${code.style.fontFamily};
+          --font-heading: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
